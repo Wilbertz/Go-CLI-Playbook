6c37c7cd5f36cc126382cce6eb43939a604db19a
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-16 19:31:22 +0200

    Go func added to main.

--- hello.go
+++ hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "Go-CLI-Playbook/hello"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
-	hello.SayHello()
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			fmt.Println(i)
+			wg.Done()
+		}()
+	}
+	wg.Wait()
 }
