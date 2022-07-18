34db4ff551af0e85cf23fe02f5ca712d4fa66848
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-07-18 20:44:17 +0200

    Profiling done.

--- mylib/mylib.go
+++ mylib/mylib.go
@@ -1,5 +1,8 @@
 package mylib
 
-func adder(l, r int) int {
-	return l + r
+import "fmt"
+
+func messageWriter(greeting, name string) string {
+	message := fmt.Sprintf("%v, %v", greeting, name)
+	return message
 }
